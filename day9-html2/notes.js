458ee5a4a11d235cc75cbfbb47ba53619264724f
githubscientist <sathish.datascientist@gmail.com>
2023-05-09 21:49:30 +0530

    day 9 completed

---
+++ day9-html2/notes.js
@@ -0,0 +1,12 @@
+/*
+    CSS - Cascading Style Sheets (Styling language)
+
+    It allows us to design the content of the web page written in HTML
+
+    Different ways to use CSS:
+
+    1. Inline CSS
+    2. Internal CSS
+    3. External CSS
+    
+*/
