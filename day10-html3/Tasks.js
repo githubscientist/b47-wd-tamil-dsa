d67debdb5068563cc70d818a8b2ff3e5b54b8fbe
githubscientist <sathish.datascientist@gmail.com>
2023-05-10 21:28:52 +0530

    day 10 completed

---
+++ day10-html3/Tasks.js
@@ -0,0 +1,12 @@
+/*
+    Task 1: Create a hover effect on a button inside a div
+        properties: background-color, color
+
+    Task 2: Create a list of links
+        - home
+        - about us
+        - contact us
+    Apply difference styles to visited and unvisited links inside a list
+
+    Task3: Change the background color of a button when it is clicked.
+*/
